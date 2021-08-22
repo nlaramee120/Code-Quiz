ff00280a432c389b31490009380af0b6a19dcb57
nlaramee120 <nlaramee120@aol.com>
2021-08-21 22:29:52 -0500

    fixed timer and started adding buttons for answers.

--- script.js
+++ script.js
@@ -1,36 +1,59 @@
 var startBtn = document.getElementById("startBtn");
-var timeLeft = document.querySelector(".timeLeft");
+var timeLeft = document.getElementById("timeLeft");
+var questionDiv = document.getElementById("question-div");
 
 
-var secondsLeft = 60
 
 function countdown() {
+
+    var secondsLeft = 60
  
     var timerInterval = setInterval(function() {
         
-        secondsLeft--;
-        timeLeft.textcontent = secondsLeft
+        timeLeft.textcontent = secondsLeft;
 
-        if(secondsLeft === 0) {
+        if (secondsLeft >= 1) {
+        secondsLeft--;
+        timeLeft.textContent = "Time Left: " + secondsLeft;
+        }
+
+       else if(secondsLeft === 0) {
             
             clearInterval(timerInterval);
 
-            sendMessage()
         }
     }, 1000);
 }
 
+// let questions = [
+//     {
+//         "text": "Commonly used data types do NOT include which of the following?",
+//         "options": ["strings", "booleans", "letters", "numbers"],
+//         "solutions": "letters"
+//     }
+//     ]
+    
+//     function displayQuestion(q) {
+//         let $text = $('<h3>').text(q("text"));
+//         $("#question-div").append($text);
+    
+//         let opts = q["options"];
+//         for (let i=0; i<opts.length; i++) {
+//             let $option = $('<button/>').attr({ value: opts[i], class: 'question-option'});
+//             $option.text(opts[i])
+//             $("#question-div").append($option);
+//         }
+    
+    
+//     }
+    
+//     displayQuestion(questions[0]);
+    
+//     console.log(displayQuestion)
 
-function sendMessage() {
-    timeLeft.textcontent = " ";
-
-}
-
-
-
+console.log(countdown)
 
 startBtn.addEventListener("click", countdown);
 
 
 
-countdown();
